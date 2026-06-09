574eb5071a3a12a7c30ec198540883dfcf105247
Benjamin <be.kullman@gmail.com>
2026-06-09 22:04:13 +0200

    Implemented Play-GameLoop function in GameEngine.psm1

--- src/modules/GameEngine.psm1
+++ src/modules/GameEngine.psm1
@@ -5,3 +5,26 @@ $currentDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 Import-Module (Join-Path $currentDir "RoomProvider.psm1") -Force
 Import-Module (Join-Path $currentDir "SaveSystem.psm1") -Force
 Import-Module (Join-Path $currentDir "ConsoleUI.psm1") -Force
+
+function Play-GameLoop ($SaveGame) {
+    $gameRunning = $true
+
+    while ($gameRunning) {
+        if ([string]::IsNullOrEmpty($SaveGame.CurrentRoomId)) {
+            $gameRunning = $false
+            break
+        }
+
+        $room = Get-RoomById -RoomId $SaveGame.CurrentRoomId
+        Show-Room -Room $room -SaveGame $SaveGame
+        $choice = Get-PlayerChoice -MaxOptions $room.Options.Count
+
+        if ($choice -eq $room.CorrectOption) {
+            Show-Feedback -IsCorrect $true -FeedbackText $room.SuccessText
+        } else {
+            Show-Feedback -IsCorrect $false -FeedbackText $room.FailureText
+        }
+
+        $SaveGame.CurrentRoomId = $room.NextRoomId
+    }
+}
